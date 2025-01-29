ebb428e3aabfc98768f814eca51aba4f1d332567
Sumon <sumonete152@gmail.com>
2025-01-30 02:42:52 +0600

    s

---
+++ loop.py
@@ -0,0 +1,15 @@
+# cars = ["Volvo", "Mercedes", "Toyota"]
+# for car in cars:
+#     print(car)
+
+# countryName = "Bangladesh"
+# for i in countryName:
+#     if i=='d': break;
+#     print(i)
+
+#print numbers untill the user input 0
+num = int(input("Enter your number as you wish: "))
+while num != 0:
+    print(f"You have entered {num}")
+    num = int(input("Enter the number: "))
+print("End")
